df39541e72486123a62801cfcd6b9915e86abd4a
rodriguez21 <34343615+rodriguez21@users.noreply.github.com>
2020-06-02 22:49:39 -0500

    conexion

---
+++ SOAT/resources/views/conexionDB.php
@@ -0,0 +1,14 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "db_soat");
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
+$mysqli = new mysqli("127.0.0.1", "usuario", "contraseña", "basedatos", 3306);
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n"; 
+?>
